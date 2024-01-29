38580045855ab2d7f2e8be22a288bdbac9c60ffa
Aliixe9 <63748139+aliixe9@users.noreply.github.com>
2024-01-28 22:23:19 -0500

    Update k3s.sh

--- Kubernetes/K3S-Deploy/k3s.sh
+++ Kubernetes/K3S-Deploy/k3s.sh
@@ -27,11 +27,11 @@ KVVERSION="v0.6.3"
 k3sVersion="v1.26.10+k3s2"
 
 # Set the IP addresses of the master and work nodes
-master1=192.168.3.21
-master2=192.168.3.22
-master3=192.168.3.23
-worker1=192.168.3.24
-worker2=192.168.3.25
+master1=192.168.2.150
+master2=192.168.2.151
+master3=192.168.2.152
+worker1=192.168.2.153
+worker2=192.168.2.154
 
 # User of remote machines
 user=ubuntu
@@ -40,7 +40,7 @@ user=ubuntu
 interface=eth0
 
 # Set the virtual IP address (VIP)
-vip=192.168.3.50
+vip=192.168.2.181
 
 # Array of master nodes
 masters=($master2 $master3)
@@ -55,7 +55,7 @@ all=($master1 $master2 $master3 $worker1 $worker2)
 allnomaster1=($master2 $master3 $worker1 $worker2)
 
 #Loadbalancer IP range
-lbrange=192.168.3.60-192.168.3.80
+lbrange=192.168.2.160-192.168.2.180
 
 #ssh certificate name variable
 certName=id_rsa
